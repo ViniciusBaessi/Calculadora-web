4539d9a733201f42d427621876e56a0f95ea16d9
Vinicius Baessi <viniciusbaessi@gmail.com>
2023-03-02 21:08:27 -0300

    save

--- assets/script.js
+++ assets/script.js
@@ -9,128 +9,56 @@ const operadores = document.querySelectorAll('.operador');
 
 //Contador
 let i = 0
+let aux
 
 var array1 = []
 var array2 = []
 
-
-
-
-let aux
-
-
-
 let valor1
-
-
-
-
-
-
-
+let valorGlobal;
 
 //Criando uma função que captura os eventos da página, no caso o clique. 
 
 //O (foreach) percorre todos os numeros da página e adiciona um ouvinte que vai capturar o clique. 
-numeros.forEach(input => {
-    input.addEventListener('click', (event) => {
-    
-        //Se houver de fato um clique, o valor do botão é armazenando na variável (botaonum) e em seguinda o valor desta variável é printado no console
 
-        const botaonum = event.target.value
-        console.log(`O botão clicado é ${botaonum}`)
-        console.clear()
+function PegarNumero () {
 
-        //Inserindo o valor do botão em um indice do array
-        array1[i] = botaonum;
+
+    numeros.forEach(input => {
+        input.addEventListener('click', (event) => {
         
+            //Se houver de fato um clique, o valor do botão é armazenando na variável (botaonum)
+            const botaonum = event.target.value
 
-        //Printando o valor do botão para que o usuário possa visualizar
-        res.innerHTML += `${botaonum}`
+            //Inserindo o valor do botão em um indice do array
+            array1[i] = botaonum;
+            
+            //Linpando o console
+            console.clear()
+
+            //Printando o valor do botão para que o usuário possa visualizar
+            res.innerHTML += `${botaonum}`
 
 
-        //Printando o valor do indice do array (printando a casinha que o dado está sendo armazenado)
-        console.log(`O valor do indice é ${i}`)
-
-
-        //Printando o dado que foi armazenado no array, que no caso é o mesmo valor do botão
-        console.log(`O valor armazenado no array é ${array1[i]}`)
-        i++
-
-
-        //Printando todos os dados do array por meio da repetição (for).
-
-        //Variável (j) é igual a zero, enquanto o valor de (j) for menor que a quantidade de dados do array, faça o print no console e (j) 
-        for (let j = 0; j < array1.length; j++) {
-            console.log(array1[j]);
-        }
+           //Atualizando o contador
+            i++
 
 
 
-        //Os valores do array serão concatenados e armazenados na variável (valor1)
+            //Os valores do array serão concatenados e armazenados na variável (valor1)
 
-        // O comando .join("") faz a concatenação e as ("") indica que não haverá espaços durante a concatenação.
-        aux = array1.join("")
+            // O comando .join("") faz a concatenação e as ("") indica que não haverá espaços durante a concatenação.
+            aux = array1.join("")
+            
+            valor1 = Number(aux)
 
-        //Convertendo os valores concatenados para number
-        valor1 = Number(aux)
-        
+                          
+            console.log(valor1)
+        });
 
-
-       
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-                        
-        
-
-
-
-
-
-        
     });
-  
-});
-
-
-
-
-
-
-
-
-
-
-
     
-
-
-
-
-
-
+}
 
 
 
@@ -148,7 +76,7 @@ function PegarOperador () {
 
             const botaooper = event.target.value
             console.log(`O botão clicado é ${botaooper}`)
-            
+
 
 
 
@@ -164,3 +92,5 @@ function PegarOperador () {
     });
 }
 
+PegarNumero ()
+PegarOperador ()
