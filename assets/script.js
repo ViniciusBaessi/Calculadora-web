a5f2ddc99f72f3ff98c6fab4de0e8b1821a290d3
Vinicius Baessi <viniciusbaessi@gmail.com>
2023-03-03 20:51:17 -0300

    save

--- assets/script.js
+++ assets/script.js
@@ -11,6 +11,7 @@ const operadores = document.querySelectorAll('.operador');
 let i = 0
 
 let aux
+let aux2
 
 let operador
 
@@ -38,16 +39,31 @@ function Clique () {
             
             val1 (botaonum)
 
+
+
+            if (operador !== undefined) {
+                botaonum = ""
+                i = 0
+                val2 (botaonum)
+            }
+
+
+
+
+
         
-            console.log(valor1)
+            console.log(`Esse é o valor1 ${valor1}`)
             console.log(`Esse é o operador ${operador}`)
             
             console.log(`Esse é o aux ${aux}`)
 
             console.log(`Esse é o i ${i}`)
 
-    
+            console.log(`Esse é o valor2 ${valor2}`)
 
+
+    
+           
 
         });
 
@@ -113,14 +129,80 @@ function val1  (a) {
     }
 
 
-
-
-    
-
   //Converte os valores para number pois estavam como string
     valor1 = Number(valor1)
     
+
+
 }
+
+
+
+
+
+
+
+
+
+function val2 (b) {
+        //Se o botão (ac) for clicado, a página será recarregada, fazendo com que os dados sejam limpos
+        if (b == "ac") {
+            b = b.replace("ac", "")
+            location.reload()
+    }
+
+    //Inserindo o valor do botão em um indice do array
+    array2[i] = b;
+    
+
+    //Atualizando o contador
+    i++
+
+    //Linpando o console
+    //console.clear()
+
+
+    //Se o usuário clicar no botão del, será excluido dois valores do array. O del prórpiamente que foi alocado de forma automática e o valor que já estava lá. 
+
+    if (b == "del") {
+        array2.splice(array2.length - 2, 2);
+        i = i-2
+    }
+
+
+    //Os valores do array serão concatenados e armazenados na letiável (valor1)
+
+    // O comando .join("") faz a concatenação e as ("") indica que não haverá espaços durante a concatenação.
+    aux2 = array2.join("")
+
+
+    valor2 = aux2
+
+
+    //Printando o valor do botão para que o usuário possa visualizar
+    res.innerHTML += `${valor2}`
+
+
+
+    //Pegando os operadores ------------------------
+    if (b == "÷" || b == "+" || b == "-" || b == "*") {
+        res.innerHTML = `Erro: Não pode executar mais de um operador`
+
+    }
+
+
+  //Converte os valores para number pois estavam como string
+    valor2 = Number(valor2)
+    
+}
+
+
+
+
+
+
+
+
 
 
 Clique ()
